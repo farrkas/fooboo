a35744dabcfd467bac857e231459e810dd4346e2
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-16 23:08:37 +0100

    zmiana pętli z for na do while

--- fooboo.js
+++ fooboo.js
@@ -1,20 +1,23 @@
-function fooboo(tab){
-const a=tab.length;
+function fooboo(tab) {
+    const a = tab.length;
 
-for (let i=0; i<a; i++){
+    do {
 
-if (typeof (tab[i])==='string'){
-console.log("foo");
-}
+        if (typeof (tab[i]) === 'string') {
+            console.log("foo");
+            i++;
+        }
 
-if (typeof tab[i]==='number'){
-    console.log("bar");
-}
-if (typeof tab[i]!=='string' && typeof tab[i]!=='number'){
-    console.log("boo");
-}
+        if (typeof tab[i] === 'number') {
+            console.log("bar");
+            i++;
+        }
+        if (typeof tab[i] !== 'string' && typeof tab[i] !== 'number') {
+            console.log("boo");
+            i++;
+        }
 
-}
+    } while (i < a);
 
 }
 const tabTest = [12, "maciek", 123, "un i uona", 3, true, 12, null];
